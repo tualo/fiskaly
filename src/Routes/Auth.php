98c4c25472ec89d19deba7d690982be22e039a36
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-07-17 11:04:20 +0200

    separate api

--- src/Routes/Auth.php
+++ src/Routes/Auth.php
@@ -1,15 +1,15 @@
 <?php
 namespace Tualo\Office\Fiskaly\Routes;
-use Tualo\Office\Basic\TualoApplication as App;
+use Tualo\Office\Basic\TualoApplication;
 use Tualo\Office\Basic\Route;
 use Tualo\Office\Basic\IRoute;
-use Tualo\Office\Fiskaly\API;
+use Tualo\Office\FiskalyAPI\API;
 
 
 class Auth implements IRoute{
     public static function register(){
         Route::add('/fiskaly/auth',function($matches){
-            App::contenttype('application/json');
+            TualoApplication::contenttype('application/json');
             try{
                 $auth = API::auth([
                     'Content-Type:application/json'
@@ -19,100 +19,104 @@ class Auth implements IRoute{
                     API::addEnvrionment('access_token_expires_at',$auth['access_token_expires_at']);
                 }
 
-                App::result('time',time() );
-                App::result('auth',$auth );
-                App::result('success',true );
-                // App::result('cashregisters',$list );
+                TualoApplication::result('time',time() );
+                TualoApplication::result('auth',$auth );
+                TualoApplication::result('success',true );
+                // TualoApplication::result('cashregisters',$list );
 
             }catch(\Exception $e){
-                App::result('msg', $e->getMessage());
+                TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
 
         Route::add('/fiskaly/cashregisters',function($matches){
-            App::contenttype('application/json');
+            TualoApplication::contenttype('application/json');
             try{
                 $list = API::getCashRegisters();
-                App::result('cashregisters',$list );
+                TualoApplication::result('cashregisters',$list );
             }catch(\Exception $e){
-                App::result('msg', $e->getMessage());
+                TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
 
         Route::add('/fiskaly/createTSS',function($matches){
-            App::contenttype('application/json');
+            TualoApplication::contenttype('application/json');
             try{
-                App::result('create', API::createTSS([
+                TualoApplication::result('create', API::createTSS([
                     'Authorization: Bearer {{access_token}}',
                     'Content-Type:application/json'
                 ]));
             }catch(\Exception $e){
-                App::result('msg', $e->getMessage());
+                TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
 
         Route::add('/fiskaly/personalizeTSS',function($matches){
-            App::contenttype('application/json');
+            TualoApplication::contenttype('application/json');
             try{
-                App::result('data', API::personalizeTSS( ));
+                TualoApplication::result('data', API::personalizeTSS( ));
             }catch(\Exception $e){
-                App::result('msg', $e->getMessage());
+                TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
 
         Route::add('/fiskaly/initializeTSS',function($matches){
-            App::contenttype('application/json');
+            TualoApplication::contenttype('application/json');
             try{
-                App::result('data', API::initializeTSS( ));
+                TualoApplication::result('data', API::initializeTSS( ));
             }catch(\Exception $e){
-                App::result('msg', $e->getMessage());
+                TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
 
         Route::add('/fiskaly/authenticateAdmin',function($matches){
-            App::contenttype('application/json');
+            TualoApplication::contenttype('application/json');
             try{
-                App::result('data', API::authenticateAdmin( ));
+                TualoApplication::result('data', API::authenticateAdmin( ));
             }catch(\Exception $e){
-                App::result('msg', $e->getMessage());
+                TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
 
         Route::add('/fiskaly/adminPin',function($matches){
-            App::contenttype('application/json');
+            TualoApplication::contenttype('application/json');
             try{
-                App::result('data', API::adminPin( ));
+                TualoApplication::result('data', API::adminPin( ));
             }catch(\Exception $e){
-                App::result('msg', $e->getMessage());
+                TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
 
 
-        Route::add('/fiskaly/createClient',function($matches){
-            App::contenttype('application/json');
+        Route::add('/fiskaly/createClient/(?P<terminalid>[\w\-\_]+)',function($matches){
+            TualoApplication::contenttype('application/json');
             try{
-                App::result('data', API::createClient( ));
+                TualoApplication::result('data', API::createClient( $matches['terminalid'] ));
             }catch(\Exception $e){
-                App::result('msg', $e->getMessage());
+                TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
 
 
         
         Route::add('/fiskaly/logoutAdmin',function($matches){
-            App::contenttype('application/json');
+            TualoApplication::contenttype('application/json');
             try{
-                App::result('data', API::logoutAdmin( ));
+                TualoApplication::result('data', API::logoutAdmin( ));
             }catch(\Exception $e){
-                App::result('msg', $e->getMessage());
+                TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
 
 
-        Route::add('/fiskaly/transaction',function($matches){
-            App::contenttype('application/json');
+        Route::add('/fiskaly/transaction/(?P<type>[\w\-\_]+)/(?P<terminalid>[\w\-\_]+)',function($matches){
+            TualoApplication::contenttype('application/json');
             try{
-                App::result('data', API::transaction([
+                TualoApplication::result('data', API::transaction(
+                    $matches['terminalid'],
+                    json_decode($_REQUEST['data'],true)
+                    /*
+                    [
                     [
                         'vat_rate' => '19',
                         'amount' => number_format(10.22,2,'.','')
@@ -121,27 +125,28 @@ class Auth implements IRoute{
                         'vat_rate' => '7',
                         'amount' => number_format(3,2,'.','')
                     ]
-                    ]/*,'RECEIPT'*/));
+                    ]*/,$matches['type']));
             }catch(\Exception $e){
-                App::result('msg', $e->getMessage());
+                TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
 
         Route::add('/fiskaly/vat_definitions',function($matches){
-            App::contenttype('application/json');
+            TualoApplication::contenttype('application/json');
             try{
-                App::result('data', API::getVatDefinitions());
+                TualoApplication::result('data', API::getVatDefinitions());
             }catch(\Exception $e){
-                App::result('msg', $e->getMessage());
+                TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
 
-        Route::add('/fiskaly/information',function($matches){
-            App::contenttype('application/json');
+        Route::add('/pos/fiskaly/information/(?P<terminalid>[\w\-\_]+)',function($matches){
+            TualoApplication::contenttype('application/json');
             try{
-                App::result('data', API::getTSSInformation());
+                TualoApplication::result('data', API::getTSSInformation($matches['terminalid']));
+                TualoApplication::result('success', true);
             }catch(\Exception $e){
-                App::result('msg', $e->getMessage());
+                TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
 
--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,4 @@
 <?php
 
-require_once "API.php";
 require_once "Routes/Auth.php";
 
